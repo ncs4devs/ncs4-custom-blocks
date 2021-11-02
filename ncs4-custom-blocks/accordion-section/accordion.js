dcfa8dac66d23bfc6bbd6aa6abea7954f72577b3
Andrew Hood <andrew.hood@usm.edu>
2021-11-02 13:18:39 -0500

    Revert "Accordion Block"
    This reverts commit cc4c5d6cb3443790e14aa220293a95eb9b0cc08b.

--- ncs4-custom-blocks/accordion-section/accordion.js
+++
@@ -1,7 +0,0 @@
-function toggleChecked(checkboxId) {
-  const checkbox = document.getElementById(checkboxId);
-  const checkboxBtn = document.getElementById( "btn-" + checkboxId);
-
-  checkbox.checked = !checkbox.checked;
-  checkbox.checked ? checkboxBtn.title = "Close topic" : checkboxBtn.title = "Expand topic";
-}
