59bda54a9b5fe94a6257525fade9240b7f262eb9
Andrew Hood <andrew.hood@usm.edu>
2021-11-17 15:38:43 -0600

    Made ColorSelector stateless

--- js/ColorSelector.js
+++ js/ColorSelector.js
@@ -11,32 +11,24 @@ export function createColorClass(slug, prop) {
 }
 
 export class ColorSelector extends React.Component {
-  constructor(props) {
-    super(props);
-    this.label = props.label;
-    this.allowGradients = Boolean(props.allowGradients); // TODO
-    this.callback = props.onChange; // {value, slug = null} => {}
-
-    this.state = {
-      value: props.value,
-    }
-  }
-
   render() {
-    let settings = select("core/block-editor").getSettings();
+    let label = this.props.label;
+    let allowGradients = this.props.allowGradients; // TODO
+    let value = this.props.value;
+    let onChange  = this.props.onChange;
 
+    let settings = select("core/block-editor").getSettings();
     return (
       <>
-        <p>{ this.label }</p>
+        <p>{ label }</p>
         <ColorPalette
           colors = { settings.colors }
           disableCustomColors = { settings.disableCustomColors }
           clearable = { !settings.disableCustomColors }
-          value = { this.state.value }
+          value = { value }
           onChange = { (c) => {
             let color = settings.colors.filter( (obj) => obj.color === c)[0];
-            this.setState( { value: c } );
-            this.callback( {
+            onChange( {
               color: c,
               slug: color ? color.slug : null,
             } );
