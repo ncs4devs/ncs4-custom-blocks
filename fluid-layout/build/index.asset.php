5c15e6132adfbef2bdb65be2966c36d1943d4712
Andrew Hood <andrew.hood@usm.edu>
2021-12-03 14:34:44 -0600

    build update

--- fluid-layout/build/index.asset.php
+++ fluid-layout/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-polyfill'), 'version' => '342ba26ec1a2a5230cf209109c79da1c');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-polyfill'), 'version' => '441769b8b349f591ea5a535a290779d9');
