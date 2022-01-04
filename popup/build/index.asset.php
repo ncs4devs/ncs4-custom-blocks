65ebcaa5ab441ba7df6375c1618ff19781f41cf2
Andrew Hood <andrew.hood@usm.edu>
2022-01-04 13:51:18 -0600

    Added textAlign: left to Popup

--- popup/build/index.asset.php
+++ popup/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-polyfill'), 'version' => '0c141d417bb64dca0dfb730deff3eb57');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-polyfill'), 'version' => 'e48d8d33fa9cb3a57a3f1cbe2be7c944');
