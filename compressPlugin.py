b67b4c8ac6f3639d4beb8315b66164abb857de0f
Andrew Hood <w10029145@usm.edu>
2022-04-14 12:26:56 -0500

    Created plugin compile script

---
+++ compressPlugin.py
@@ -0,0 +1,83 @@
+import zipfile
+import re
+import functools
+import os
+import sys
+
+def patternToRegex(pattern: str) -> str:
+    regex = re.sub("\.", "\.", pattern)
+    regex = re.sub("\*", ".*", regex)
+    return regex
+
+
+def createIncludeChecker(filePath: str) -> lambda str: bool:
+    with open(filePath) as patternFile:
+        patterns = patternFile.readlines()
+        def checker(f):
+            if f == filePath:
+                return False
+            for pattern in patterns:
+                pattern = pattern.rstrip("\n")
+                pattern = patternToRegex(pattern)
+                if pattern.isspace() or pattern == "":
+                    continue
+                if bool(re.search(pattern, f)):
+                    return False
+            return True
+
+        return checker
+
+def getFileList(dir: str) -> list[str]:
+    for (path, _, fileNames) in os.walk(dir):
+        for file in fileNames:
+            yield os.path.join(path, file).replace("\\", "/")
+
+
+def makeZip(dir: str, zipFile: str, ignoreFile: str) -> None:
+    checker = createIncludeChecker(ignoreFile)
+    files = filter(checker, getFileList(dir))
+    with zipfile.ZipFile(zipFile, "w") as zip:
+        for file in files:
+            print(file)
+            zip.write(file);
+
+args = {
+    "dir": "./",
+    "ignoreFile": "./.buildignore",
+    "zipFile": "../ncs4-custom-blocks.zip",
+}
+
+flags = {
+    "ignoreFile": "--ignore",
+    "zipFile": "--out",
+}
+
+def tokenizeArgs(args: dict, flags: dict) -> list[dict]:
+    tokens = []
+    i = 1 # arg 0 is the program name
+    while i < len(args):
+        if args[i] in flags.values():
+            if i + 1 < len(args):
+                tokens.append({ "flag": args[i], "arg": args[i+1] })
+            else:
+                raise SyntaxError(
+                    "Argument expected for option '" + args[i] + "'"
+                )
+            i += 1
+        else:
+            tokens.append({ "flag": None, "arg": args[i] })
+        i += 1
+    return tokens
+
+for token in tokenizeArgs(sys.argv, flags):
+    if token["flag"] == None:
+        args["dir"] = token["arg"]
+    else:
+        for option, flag in flags.items():
+            if flag == token["flag"]:
+                args[option] = token["arg"]
+                break
+        raise NotImplementedError
+
+
+makeZip(**args);
