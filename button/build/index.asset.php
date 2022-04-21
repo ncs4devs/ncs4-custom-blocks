c5003729c4550f672272e00957cb9d49b54ddac9
Andrew Hood <w10029145@usm.edu>
2022-04-21 14:26:49 -0500

    Rebuild

--- button/build/index.asset.php
+++ button/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-polyfill'), 'version' => '739314bb293be8cfe90480a469733fab');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-polyfill'), 'version' => 'b6dc7250f65b80afd51412827aed8804');
