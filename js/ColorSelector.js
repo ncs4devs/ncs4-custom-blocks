fa4149c50cdfe03cd4d09e34b48954aeb33a6b58
Andrew Hood <w10029145@usm.edu>
2022-05-19 11:13:11 -0500

    Refactoring; Remove unnecessary exports; create useColor hook

--- js/ColorSelector.js
+++ js/ColorSelector.js
@@ -1,32 +1,9 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 
 import { select } from '@wordpress/data';
 import { ColorPalette } from '@wordpress/components';
 
-// Adds className and style properties to a color loaded from DB
-export function fromColorAttribute(attr, bg = false) {
-  let customColor = attr.slug ? null : attr.color;
-
-  if (bg) {
-    return Object.assign({}, attr, {
-      className: createColorClass(attr.slug, "background-color"),
-      style: {
-        backgroundColor: customColor,
-        ["--palette-bg-color"]: customColor,
-      },
-    });
-  }
-
-  return Object.assign({}, attr, {
-    className: createColorClass(attr.slug, "color"),
-    style: {
-      color: customColor,
-      ["--palette-color"]: customColor,
-    },
-  });
-}
-
-export function createColorClass(slug, prop) {
+function createColorClass(slug, prop) {
   if (!slug || !prop) {
     return null;
   }
@@ -34,62 +11,101 @@ export function createColorClass(slug, prop) {
 }
 
 // creates a style dictionary suitable for inline styling of custom colors
-/* input format:
-  [
-    { color: attrs.bgColor,
-      props: [ "background-color", "--palette-bg-color"],
-    },
-    { color: attrs.textColor,
-      props: [ "color", "--palette-color" ],
-    },
-  ]
-*/
-export function createColorStyle(attributes) {
+function createColorStyle(colorData, isBg) {
   let style = {};
-  for (let attr of attributes) {
-    if (!attr.color) { continue }
-    let color = attr.color.slug ? null : attr.color.color;
-    for (let prop of attr.props) {
-      style[prop] = color;
-    }
+  if (colorData.slug) {
+    return style; // color is not custom, so no need for inline style
   }
+
+  if (isBg) {
+    style.backgroundColor = colorData.color;
+    style["--palette-bg-color"] = colorData.color;
+  } else {
+    style.color = colorData.color;
+    style["--palette-color"] = colorData.color;
+  }
+
   return style;
-}
-
-export class ColorSelector extends React.Component {
-  render() {
-    let label = this.props.label;
-    let allowGradients = this.props.allowGradients; // TODO
-    let value = this.props.value;
-    let onChange  = this.props.onChange;
-
-    let settings = select("core/block-editor").getSettings();
-    return (
-      <>
-        <p>{ label }</p>
-        <ColorPalette
-          colors = { settings.colors }
-          disableCustomColors = { settings.disableCustomColors }
-          value = { value }
-          onChange = { (c) => {
-            let color = settings.colors.filter( (obj) => obj.color === c)[0];
-            onChange( {
-              color: c,
-              slug: color ? color.slug : null,
-            } );
-          }}
-        />
-      </>
-    );
-  }
-}
-
-export function verifyColor(color) {
-  let colors = select("core/block-editor").getSettings().colors;
-  return getColorBySlug(color.slug, colors);
 }
 
 function getColorBySlug(slug, colors) {
   let color = colors.filter( (obj) => obj.slug === slug)[0];
   return color ? color.color : null;
 }
+
+function getSlugByColor(color, colors) {
+  let c = colors.filter( (obj) => obj.color === color)[0];
+  return c ? c : null;
+}
+
+function matchColor(color, colors) {
+  if (!color || typeof color !== "object") {
+    console.warn("Malformed color");
+    return {color: null, slug: null};
+  }
+
+  for (let c of colors) {
+    if (
+         (color.color && c.color === color.color)
+      || (color.slug && c.slug === color.slug)
+    ) {
+      return {color: c.color, slug: c.slug};
+    }
+  }
+
+  if (color.color && !color.slug) {
+    return color; // this is a custom color
+  }
+
+  console.warn("Unknown color:", color);
+  return {color: null, slug: null};
+}
+
+const ColorSelector = (props) => {
+  let settings = select("core/block-editor").getSettings();
+
+  return <>
+    <p>{ props.label }</p>
+    <ColorPalette
+      colors = { settings.colors }
+      disableCustomColors = { settings.disableCustomColors }
+      value = { props.value }
+      onChange = { (c) => {
+        let color = getSlugByColor(c, settings.colors);
+        props.onChange( {
+          color: c,
+          slug: color ? color.slug : null,
+        } );
+      } }
+    />
+  </>
+}
+
+// adds additional color data, e.g. classes, styles, etc
+export const fromColorAttribute = (colorAttr, isBg) => ({
+  data: colorAttr,
+  className: isBg
+    ? createColorClass(colorAttr.slug, "background-color")
+    : createColorClass(colorAttr.slug, "color"),
+  style: createColorStyle(colorAttr, isBg),
+});
+
+export function useColor(colorAttr, isBg, callback) {
+  let data = {};
+  let colors = select("core/block-editor").getSettings().colors;
+  let [color, setColor] = useState({}); // rich color data
+
+  useEffect( () => {
+    // reconstruct color slug and code from retrieved value
+    data = matchColor(colorAttr, colors);
+    if (typeof callback === "function") {
+      callback(data); // save to state
+    }
+    // Add extra data for classes, styles, etc
+    setColor(fromColorAttribute(data, isBg));
+  }, [colorAttr.color, colorAttr.slug] );
+
+  return color;
+}
+
+export default ColorSelector;
